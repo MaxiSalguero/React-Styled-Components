f8a07b7de14697ff1399dec2252e5cd2d6928341
Maximiliano Salguero <you@example.com>
2023-06-11 06:59:45 -0300

    Aula 3 Finalizada - Herencia de Styles, Buenas practicas CSS, Media query en styled-components

--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -1,21 +1,11 @@
-.container {
-  background-color: #f1f1f1;
-  min-height: 90vh;
-  padding: 0px 15vw;
-}
+import styled from "styled-components";
 
-.content {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-
-.imagen-icono {
+export const Icono = styled.img `
   height: 25px;
   width: 25px;
-}
+`
 
-.btn {
+export const Btn = styled.button `
   margin: 15px auto 0px auto;
   display: block;
   border-radius: 20px;
@@ -26,9 +16,9 @@
   font-size: 14px;
   padding: 8px 20px;
   cursor: pointer;
-}
+`
 
-.box {
+export const Box = styled.div `
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -37,25 +27,17 @@
   box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.04);
   padding: 20px;
   width: 48%;
-}
-
-.saldo {
-  font-weight: 700;
-  font-size: 32px;
-}
-
-.detalle {
-  color: #41d3be;
-  font-size: 24px;
-}
-
-@media (max-width: 800px) {
-  .box {
+  @media (max-width: 800px) {
     width: 95%;
     margin: 5px;
   }
+`
+export const Saldo = styled.div `
+  font-weight: 700;
+  font-size: 32px;
+`
 
-  .content {
-    flex-direction: column;
-  }
-}
+export const Detalle = styled.span `
+  color: #41d3be;
+  font-size: 24px;
+`
